071929daf2d17952baed72bc30fd6364f3ac1387
Ivan <gureff.al@yandex.ru>
2024-04-16 16:21:49 +0300

    Add new particle's

--- Assets/Source/Scripts/Movement.cs
+++ Assets/Source/Scripts/Movement.cs
@@ -5,6 +5,10 @@ public class Movement : MonoBehaviour
     [SerializeField] private Rigidbody _rigidBodyOfRocket;
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private AudioClip _thrustEngineClip;
+    [SerializeField] private ParticleSystem _mainEngineParticle;
+    [SerializeField] private ParticleSystem _leftSideEngineParticle;
+    [SerializeField] private ParticleSystem _rightSideEngineParticle;
+    [SerializeField] private ParticleSystem _middleSideEngineParticle;
     [Space]
     [SerializeField] private float _mainTrustPower = 1000f;
     [SerializeField] private float _rotationTrustPower = 100f;
@@ -19,16 +23,18 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            _rigidBodyOfRocket.AddRelativeForce(Vector3.up * _mainTrustPower * Time.deltaTime);
+            _rigidBodyOfRocket.AddRelativeForce(Vector3.up * _mainTrustPower * Time.deltaTime);          
 
             if (_audioSource.isPlaying is false)
             {
                 _audioSource.PlayOneShot(_thrustEngineClip);
+                _mainEngineParticle.Play();
             }
         }
         else if (_audioSource.isPlaying)
         {
             _audioSource.Stop();
+            _mainEngineParticle.Stop();
         }
     }
 
@@ -37,11 +43,21 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             ApplyRotation(_rotationTrustPower);
+            StartParticlePlay(_leftSideEngineParticle);
+            StartParticlePlay(_middleSideEngineParticle);         
         }
         else if (Input.GetKey(KeyCode.D))
         {
             ApplyRotation(-_rotationTrustPower);
+            StartParticlePlay(_rightSideEngineParticle);
+            StartParticlePlay(_middleSideEngineParticle);           
         }
+        else
+        {
+            _leftSideEngineParticle.Stop();
+            _rightSideEngineParticle.Stop();
+            _middleSideEngineParticle.Stop();
+        }    
     }
 
     private void ApplyRotation(float rotationThisFrame)
@@ -49,5 +65,13 @@ public class Movement : MonoBehaviour
         _rigidBodyOfRocket.freezeRotation = true; //freezing rotation so we can manually rotate
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
         _rigidBodyOfRocket.freezeRotation = false; //unfreezing rotation so the physics system can take over
+    }
+
+    private void StartParticlePlay(ParticleSystem particle)
+    {
+        if (particle.isPlaying is false)
+        {
+            particle.Play();
+        }
     }
 }
